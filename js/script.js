57324fb0957227de58a5a6cf17626803df1ddab6
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-05-08 17:44:26 -0300

    seção de dados animado e estável

--- js/script.js
+++ js/script.js
@@ -42,9 +42,6 @@ function showAnswerTwo() {
     item.classList.remove("active");
   });
   answers[1].classList.add("active");
-  line.forEach(function (item) {
-    console.log(item);
-  });
   line[1].classList.add("active");
   icon.forEach(function (item) {
     item.classList.remove("question-active");
@@ -63,9 +60,6 @@ function showAnswerThree() {
     item.classList.remove("active");
   });
   answers[2].classList.add("active");
-  line.forEach(function (item) {
-    console.log(item);
-  });
   line[2].classList.add("active");
   icon.forEach(function (item) {
     item.classList.remove("question-active");
@@ -84,9 +78,6 @@ function showAnswerFour() {
     item.classList.remove("active");
   });
   answers[3].classList.add("active");
-  line.forEach(function (item) {
-    console.log(item);
-  });
   line[3].classList.add("active");
   icon.forEach(function (item) {
     item.classList.remove("question-active");
@@ -137,19 +128,153 @@ function showAnswerMobileFour() {
 
 // Data
 
+const dataInfos = document.querySelectorAll(".data-right p");
+const toTop = window.innerHeight * 0.9;
 const numbers = document.querySelectorAll("[data-numbers]");
 
-numbers.forEach(function (item) {
-  const numbersClean = +item.textContent;
-  const increment = Math.floor(numbersClean / 20);
+function animaScroll() {
+  const viewTop = dataInfos[0].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[0].classList.add("active-scroll");
+  }
+}
 
-  let start = 0;
-  const timer = setInterval(function () {
-    start = start + increment;
-    item.textContent = start;
-    if (start > numbersClean) {
-      item.textContent = numbersClean;
-      clearInterval(timer);
-    }
-  }, 100 * Math.random());
-});
+window.addEventListener("scroll", animaScroll);
+
+function animaScrollTwo() {
+  const viewTop = dataInfos[1].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[1].classList.add("active-scroll");
+    window.removeEventListener("scroll", animaScrollTwo);
+    const total = +numbers[0].textContent;
+    const increment = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + increment;
+      numbers[0].textContent = start;
+      if (start > total) {
+        numbers[0].textContent = total;
+        clearInterval(timer);
+      }
+    }, 1);
+  }
+}
+
+window.addEventListener("scroll", animaScrollTwo);
+
+function animaScrollThree() {
+  const viewTop = dataInfos[2].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[2].classList.add("active-scroll");
+    window.removeEventListener("scroll", animaScrollThree);
+    const total = +numbers[1].textContent;
+    const increment = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + increment;
+      numbers[1].textContent = start;
+      if (start > total) {
+        numbers[1].textContent = total;
+        clearInterval(timer);
+      }
+    }, 1);
+  }
+}
+
+window.addEventListener("scroll", animaScrollThree);
+
+function animaScrollFour() {
+  const viewTop = dataInfos[2].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[2].classList.add("active-scroll");
+    window.removeEventListener("scroll", animaScrollFour);
+    const total = +numbers[2].textContent;
+    const increment = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + increment;
+      numbers[2].textContent = start;
+      if (start > total) {
+        numbers[2].textContent = total;
+        clearInterval(timer);
+      }
+    }, 1);
+  }
+}
+
+window.addEventListener("scroll", animaScrollFour);
+
+function animaScrollFive() {
+  const viewTop = dataInfos[3].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[3].classList.add("active-scroll");
+    window.removeEventListener("scroll", animaScrollFive);
+    const total = +numbers[3].textContent;
+    const increment = Math.floor(total / 20);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + increment;
+      numbers[3].textContent = start;
+      if (start > total) {
+        numbers[3].textContent = total;
+        clearInterval(timer);
+      }
+    }, 1);
+  }
+}
+
+window.addEventListener("scroll", animaScrollFive);
+
+function animaScrollSix() {
+  const viewTop = dataInfos[3].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[3].classList.add("active-scroll");
+    window.removeEventListener("scroll", animaScrollSix);
+    const total = +numbers[4].textContent;
+    const increment = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + increment;
+      numbers[4].textContent = start;
+      if (start > total) {
+        numbers[4].textContent = total;
+        clearInterval(timer);
+      }
+    }, 1);
+  }
+}
+
+window.addEventListener("scroll", animaScrollSix);
+
+function animaScrollSeven() {
+  const viewTop = dataInfos[4].getBoundingClientRect().top - toTop;
+  if (viewTop < 0) {
+    dataInfos[4].classList.add("active-scroll");
+    window.removeEventListener("scroll", animaScrollSeven);
+    const total = +numbers[5].textContent;
+    const increment = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start = start + increment;
+      numbers[5].textContent = start;
+      if (start > total) {
+        numbers[5].textContent = total;
+        clearInterval(timer);
+      }
+    }, 1);
+  }
+}
+
+window.addEventListener("scroll", animaScrollSeven);
+
+const img = document.querySelector(".data-left img");
+const toTopImg = window.innerHeight * 0.8;
+
+function animaScrollImg() {
+  const viewTop = img.getBoundingClientRect().top - toTopImg;
+  if (viewTop < 0) {
+    img.classList.add("show-img");
+  }
+}
+
+window.addEventListener("scroll", animaScrollImg);
